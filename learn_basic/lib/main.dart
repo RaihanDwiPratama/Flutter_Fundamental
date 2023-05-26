76ceddd42545df4d2dc94b394b35d22b584a06ef
raihan <raihanp0308@gmail.com>
2023-05-26 16:47:18 +0700

    Update

--- learn_basic/lib/main.dart
+++ learn_basic/lib/main.dart
@@ -6,23 +6,69 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home tampilan awal aplikasi
-      // scaffold menyediakan ada header dan bottom
       home: Scaffold(
-        // appbar buat bar tampilan dari aplokasi
         appBar: AppBar(
-          // buat nampilin judul app bar
-          title: Text("Aplikasi Hello World!"),
+          title: Text("Second screen"),
+          centerTitle: true,
         ),
-        // bikin konten didalam body, rata tengah
-        body: Center(
-          // konten didalam body
-          child: Text("Hello World!"),
-        ),
+        body: (Container(
+          child: RichText(
+              text: TextSpan(
+                  text: "Welcome\n",
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                TextSpan(
+                  text: "John Doe",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Colors.black),
+                ),
+              ])),
+        )),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// // fungsi utama yang akan dipanggil pertama kali
+// void main() {
+//   runApp(new MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       // home tampilan awal aplikasi
+//       // scaffold menyediakan ada header dan bottom
+//       home: Scaffold(
+//         // appbar buat bar tampilan dari aplokasi
+//         appBar: AppBar(
+//           // buat nampilin judul app bar
+//           title: Text("Aplikasi Hello World!"),
+//         ),
+//         // bikin konten didalam body, rata tengah
+//         body: Center(
+//           // konten didalam body
+//           child: Container(
+//               width: 150,
+//               height: 100,
+//               color: Colors.orange.shade700,
+//               child: Text(
+//                 "Aku pasti bisa jadi expert di flutter",
+//                 style: TextStyle(
+//                     fontSize: 20,
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.w700,)
+//               )),
+//         ),
+//       ),
+//     );
+//   }
+// }
