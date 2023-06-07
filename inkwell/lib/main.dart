0c6d0ca323352cde9180e456c3c97d19b10f0871
raihan <raihanp0308@gmail.com>
2023-06-07 14:56:30 +0700

    Inkwell Button

--- inkwell/lib/main.dart
+++ inkwell/lib/main.dart
@@ -38,6 +38,45 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+              Container(
+                margin: EdgeInsets.only(top: 40, bottom: 10),
+                child: Text(
+                  "Inkwell Button Example",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+              ),
+              // add elevation
+              Material(
+                borderRadius: BorderRadius.circular(20),
+                elevation: 2,
+                child: Container(
+                  width: 200,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [Colors.teal, Colors.lightGreen],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {},
+                      splashColor: Colors.lightGreen[400],
+                      borderRadius: BorderRadius.circular(20),
+                      child: Center(
+                        child: Text(
+                          "Inkwel Button",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
